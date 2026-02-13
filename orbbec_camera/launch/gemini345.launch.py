766b7b99181c0191904369b487e800262b844156
ob-yalian <luotao2025@163.com>
2026-02-13 15:57:44 +0800

    feat: add preset_firmware_path launch argument to multiple launch files

--- orbbec_camera/launch/gemini345.launch.py
+++ orbbec_camera/launch/gemini345.launch.py
@@ -74,6 +74,7 @@ def generate_launch_description():
         DeclareLaunchArgument('usb_port', default_value=''),
         DeclareLaunchArgument('device_num', default_value='1'),
         DeclareLaunchArgument('upgrade_firmware', default_value=''),
+        DeclareLaunchArgument('preset_firmware_path', default_value=''),
         DeclareLaunchArgument('load_config_json_file_path', default_value=''),
         DeclareLaunchArgument('export_config_json_file_path', default_value=''),
         DeclareLaunchArgument('uvc_backend', default_value='libuvc'),#libuvc or v4l2
