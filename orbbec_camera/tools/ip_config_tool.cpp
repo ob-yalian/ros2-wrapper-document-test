1421049cbf0282204b2e58505e0fa5a3ddff34a8
ob-yalian <luotao2025@163.com>
2026-03-25 14:05:31 +0800

    refactor(ip_config_tool): simplify force-ip

--- orbbec_camera/tools/ip_config_tool.cpp
+++ orbbec_camera/tools/ip_config_tool.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <chrono>
+#include <thread>
 
 using namespace ob;
 
@@ -36,39 +38,45 @@ bool isParamProvided(int argc, char **argv, const std::string &key) {
 }
 
 void printHelp() {
-  std::cout << "Usage:\n"
-            << "  ros2 run orbbec_camera ip_config_tool --ros-args [params]\n"
-            << "  (legacy alias: set_device_ip)\n\n"
-            << "Parameters:\n"
-            << "  -p old_ip:=<ip>            Current device IP (default: 192.168.1.10)\n"
-            << "  -p port:=<port>            Device port (default: 8090)\n"
-            << "  -p enable_lla:=<bool>      Set LLA switch directly (true: enable, false: disable, default: false)\n"
-            << "                              Note: LLA is applied only when this parameter is explicitly provided.\n"
-            << "  -p enable_set_ip:=<bool>   Enable set-ip operation (default: false)\n"
-            << "  -p dhcp:=<bool>            DHCP flag for set-ip/force-ip config (default: false)\n"
-            << "  -p new_ip:=<ip>            Static IP for set-ip/force-ip (default: 192.168.1.200)\n"
-            << "  -p mask:=<ip>              Subnet mask for set-ip/force-ip (default: 255.255.255.0)\n"
-            << "  -p gateway:=<ip>           Gateway for set-ip/force-ip (default: 192.168.1.1)\n"
-            << "  -p enable_force_ip:=<bool> Enable force-ip operation (default: false)\n"
-            << "  -p force_ip_mac:=<mac>     Target MAC for force-ip (optional, e.g. 54:14:FD:06:07:DA)\n\n"
-            << "Examples:\n"
-            << "\n"
-            << "  [LLA]\n"
-            << "    enable:  ros2 run orbbec_camera ip_config_tool --ros-args -p old_ip:=192.168.1.10 -p enable_lla:=true\n"
-            << "    disable: ros2 run orbbec_camera ip_config_tool --ros-args -p old_ip:=192.168.1.10 -p enable_lla:=false\n"
-            << "\n"
-            << "  [Set IP]\n"
-            << "    DHCP:    ros2 run orbbec_camera ip_config_tool --ros-args \\\n"
-            << "             -p old_ip:=192.168.1.10 -p enable_set_ip:=true -p dhcp:=true\n"
-            << "    Static:  ros2 run orbbec_camera ip_config_tool --ros-args \\\n"
-            << "             -p old_ip:=192.168.1.10 -p enable_set_ip:=true -p dhcp:=false \\\n"
-            << "             -p new_ip:=192.168.1.200 -p mask:=255.255.255.0 -p gateway:=192.168.1.1\n"
-            << "\n"
-            << "  [Force IP]\n"
-            << "    by MAC:  ros2 run orbbec_camera ip_config_tool --ros-args \\\n"
-            << "             -p enable_force_ip:=true \\\n"
-            << "             -p force_ip_mac:=54:14:FD:06:07:DA -p dhcp:=false \\\n"
-            << "             -p new_ip:=192.168.1.200 -p mask:=255.255.255.0 -p gateway:=192.168.1.1\n";
+  std::cout
+      << "Usage:\n"
+      << "  ros2 run orbbec_camera ip_config_tool --ros-args [params]\n"
+      << "  (legacy alias: set_device_ip)\n\n"
+      << "Parameters:\n"
+      << "  -p old_ip:=<ip>            Current device IP (default: 192.168.1.10)\n"
+      << "  -p port:=<port>            Device port (default: 8090)\n"
+      << "  -p enable_lla:=<bool>      Set LLA switch directly (true: enable, false: disable, "
+         "default: false)\n"
+      << "                              Note: LLA is applied only when this parameter is "
+         "explicitly provided.\n"
+      << "  -p enable_set_ip:=<bool>   Enable set-ip operation (default: false)\n"
+      << "  -p dhcp:=<bool>            DHCP flag for set-ip/force-ip config (default: false)\n"
+      << "  -p new_ip:=<ip>            Static IP for set-ip/force-ip (default: 192.168.1.200)\n"
+      << "  -p mask:=<ip>              Subnet mask for set-ip/force-ip (default: 255.255.255.0)\n"
+      << "  -p gateway:=<ip>           Gateway for set-ip/force-ip (default: 192.168.1.1)\n"
+      << "  -p enable_force_ip:=<bool> Enable force-ip operation (default: false)\n"
+      << "  -p force_ip_mac:=<mac>     Target MAC for force-ip (required, e.g. "
+         "54:14:FD:06:07:DA)\n\n"
+      << "Examples:\n"
+      << "\n"
+      << "  [LLA]\n"
+      << "    enable:  ros2 run orbbec_camera ip_config_tool --ros-args -p old_ip:=192.168.1.10 -p "
+         "enable_lla:=true\n"
+      << "    disable: ros2 run orbbec_camera ip_config_tool --ros-args -p old_ip:=192.168.1.10 -p "
+         "enable_lla:=false\n"
+      << "\n"
+      << "  [Set IP]\n"
+      << "    DHCP:    ros2 run orbbec_camera ip_config_tool --ros-args \\\n"
+      << "             -p old_ip:=192.168.1.10 -p enable_set_ip:=true -p dhcp:=true\n"
+      << "    Static:  ros2 run orbbec_camera ip_config_tool --ros-args \\\n"
+      << "             -p old_ip:=192.168.1.10 -p enable_set_ip:=true -p dhcp:=false \\\n"
+      << "             -p new_ip:=192.168.1.200 -p mask:=255.255.255.0 -p gateway:=192.168.1.1\n"
+      << "\n"
+      << "  [Force IP]\n"
+      << "    by MAC:  ros2 run orbbec_camera ip_config_tool --ros-args \\\n"
+      << "             -p enable_force_ip:=true \\\n"
+      << "             -p force_ip_mac:=54:14:FD:06:07:DA -p dhcp:=false \\\n"
+      << "             -p new_ip:=192.168.1.200 -p mask:=255.255.255.0 -p gateway:=192.168.1.1\n";
 }
 
 int main(int argc, char **argv) {
@@ -101,7 +109,8 @@ int main(int argc, char **argv) {
 
   if (!do_lla && !enable_set_ip && !enable_force_ip) {
     RCLCPP_ERROR(logger,
-                 "No operation enabled. Please enable at least one of: enable_lla, enable_set_ip, enable_force_ip.");
+                 "No operation enabled. Please enable at least one of: enable_lla, enable_set_ip, "
+                 "enable_force_ip.");
     rclcpp::shutdown();
     return 1;
   }
@@ -136,9 +145,11 @@ int main(int argc, char **argv) {
       auto device = context->createNetDevice(device_ip_str.c_str(), port);
 
       if (do_lla) {
-        if (device->isPropertySupported(OB_PROP_DEVICE_NETWORK_LLA_BOOL, OB_PERMISSION_READ_WRITE)) {
+        if (device->isPropertySupported(OB_PROP_DEVICE_NETWORK_LLA_BOOL,
+                                        OB_PERMISSION_READ_WRITE)) {
           device->setBoolProperty(OB_PROP_DEVICE_NETWORK_LLA_BOOL, enable_lla);
-          RCLCPP_INFO(logger, "LLA set successfully. target=%s", enable_lla ? "enabled" : "disabled");
+          RCLCPP_INFO(logger, "LLA set successfully. target=%s",
+                      enable_lla ? "enabled" : "disabled");
         } else {
           RCLCPP_WARN(logger, "LLA property is not supported on this device.");
         }
@@ -157,10 +168,10 @@ int main(int argc, char **argv) {
         } else {
           RCLCPP_INFO(logger, "Set-ip target static IP: %d.%d.%d.%d", ip_config.address[0],
                       ip_config.address[1], ip_config.address[2], ip_config.address[3]);
-          RCLCPP_INFO(logger, "Set-ip target mask: %d.%d.%d.%d", ip_config.mask[0], ip_config.mask[1],
-                      ip_config.mask[2], ip_config.mask[3]);
-          RCLCPP_INFO(logger, "Set-ip target gateway: %d.%d.%d.%d", ip_config.gateway[0], ip_config.gateway[1],
-                      ip_config.gateway[2], ip_config.gateway[3]);
+          RCLCPP_INFO(logger, "Set-ip target mask: %d.%d.%d.%d", ip_config.mask[0],
+                      ip_config.mask[1], ip_config.mask[2], ip_config.mask[3]);
+          RCLCPP_INFO(logger, "Set-ip target gateway: %d.%d.%d.%d", ip_config.gateway[0],
+                      ip_config.gateway[1], ip_config.gateway[2], ip_config.gateway[3]);
         }
       } else {
         RCLCPP_INFO(logger, "Set-ip operation skipped (enable_set_ip=false).");
@@ -168,47 +179,16 @@ int main(int argc, char **argv) {
     }
 
     if (enable_force_ip) {
-      std::string mac = force_ip_mac;
-      if (mac.empty()) {
-        auto list = context->queryDeviceList();
-        size_t ethernet_count = 0;
-        std::string single_ethernet_mac;
-        std::string mac_by_old_ip;
-
-        for (size_t i = 0; i < list->deviceCount(); ++i) {
-          if (std::string(list->getConnectionType(i)) != "Ethernet") {
-            continue;
-          }
-          ++ethernet_count;
-          const std::string cur_mac = list->getUid(i);
-          const std::string cur_ip = list->getIpAddress(i);
-          if (ethernet_count == 1) {
-            single_ethernet_mac = cur_mac;
-          }
-          if (!device_ip_str.empty() && cur_ip == device_ip_str) {
-            mac_by_old_ip = cur_mac;
-          }
-        }
-
-        if (!mac_by_old_ip.empty()) {
-          mac = mac_by_old_ip;
-          RCLCPP_INFO(logger, "force_ip_mac not provided, selected MAC by old_ip(%s): %s",
-                      device_ip_str.c_str(), mac.c_str());
-        } else if (ethernet_count == 1) {
-          mac = single_ethernet_mac;
-          RCLCPP_INFO(logger, "force_ip_mac not provided, selected the only Ethernet device MAC: %s",
-                      mac.c_str());
-        } else {
-          RCLCPP_ERROR(logger,
-                       "force_ip_mac is required when multiple Ethernet devices exist and old_ip does not match any device.");
-          rclcpp::shutdown();
-          return 1;
-        }
+      if (force_ip_mac.empty()) {
+        RCLCPP_ERROR(logger, "force_ip_mac is required when enable_force_ip=true.");
+        rclcpp::shutdown();
+        return 1;
       }
 
-      RCLCPP_INFO(logger, "Applying force-ip to MAC %s ...", mac.c_str());
-      if (context->forceIp(mac.c_str(), ip_config)) {
+      RCLCPP_INFO(logger, "Applying force-ip to MAC %s ...", force_ip_mac.c_str());
+      if (context->forceIp(force_ip_mac.c_str(), ip_config)) {
         RCLCPP_INFO(logger, "Force-ip operation applied successfully.");
+        std::this_thread::sleep_for(std::chrono::seconds(5));
         if (dhcp) {
           RCLCPP_INFO(logger, "Force-ip target mode: DHCP.");
         } else {
