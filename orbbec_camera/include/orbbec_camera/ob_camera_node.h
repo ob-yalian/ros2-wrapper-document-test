1b38de0d09e5c66bc4729ea5711f9462ffb2caf1
ob-yalian <luotao2025@163.com>
2026-06-05 11:07:37 +0800

    Revert "feat: add parameter synchronization and validation methods in Parameters and OBCameraNode classes"
    This reverts commit c086004c19590e32bf66549272b1e2571815d002.

--- orbbec_camera/include/orbbec_camera/ob_camera_node.h
+++ orbbec_camera/include/orbbec_camera/ob_camera_node.h
@@ -552,9 +552,6 @@ class OBCameraNode {
   void setupLeftIrPostProcessFilter();
   void setupUndistortionFilters();
   bool shouldUseHwD2CColorUndistortion() const;
-  bool isHwD2CProfileSupported() const;
-  bool shouldUseGeneratedCameraInfo(const stream_index_pair& stream_index) const;
-  std::string getEffectiveOpticalFrameId(const stream_index_pair& stream_index) const;
   void configureHwD2CColorUndistortion(const std::shared_ptr<ob::Frame>& depth_frame);
   void applyHwD2CColorUndistortion(std::shared_ptr<ob::FrameSet>& frame_set,
                                    const std::shared_ptr<ob::Frame>& depth_frame);
